12cf3a02095166212d31fe42de00c9b9739317b5
karata <karata.brown@outlook.com>
2014-09-11 14:58:00 +0800

    Add Writer Sample project

---
+++ Writer/ODataSamples.Writer/Message.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.OData.Core;
+
+namespace Demo1
+{
+    public class Message : IODataRequestMessage, IODataResponseMessage, IDisposable
+    {
+        private readonly Dictionary<string, string> headers;
+
+        public Message()
+        {
+            headers = new Dictionary<string, string>();
+        }
+
+        public IEnumerable<KeyValuePair<string, string>> Headers
+        {
+            get { return this.headers; }
+        }
+
+        public int StatusCode { get; set; }
+
+        public Uri Url { get; set; }
+
+        public string Method { get; set; }
+
+        public Stream Stream { get; set; }
+
+        public string GetHeader(string headerName)
+        {
+            string headerValue;
+            return this.headers.TryGetValue(headerName, out headerValue) ? headerValue : null;
+        }
+
+        public void SetHeader(string headerName, string headerValue)
+        {
+            headers[headerName] = headerValue;
+        }
+
+        public Stream GetStream()
+        {
+            return this.Stream;
+        }
+
+        public Action DisposeAction { get; set; }
+
+        void IDisposable.Dispose()
+        {
+            if (this.DisposeAction != null)
+            {
+                this.DisposeAction();
+            }
+        }
+    }
+}
---
+++ Writer/ODataSamples.Writer/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo1;
+using Microsoft.OData.Core;
+using ODataSamples.Common.Model;
+
+namespace ODataSamples.Writer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var extModel = new ParserExtModel();
+            var stream = new MemoryStream();
+            var msg = new Message()
+            {
+                Stream = stream,
+            };
+
+            var settings = new ODataMessageWriterSettings()
+            {
+                ODataUri = new ODataUri()
+                {
+                    ServiceRoot = new Uri("http://host/svc")
+                },
+                DisableMessageStreamDisposal = true,
+                JsonPCallback = "fsjflsajflsjalfjas",
+            };
+
+            ODataFeed feed = new ODataFeed();
+            ODataEntry entry = new ODataEntry();
+            entry.TypeName = "TestNS.Fish";
+            var ppr = new List<ODataProperty>();
+            ppr.Add(new ODataProperty()
+            {
+                Name = "Color",
+                Value = new ODataEnumValue("blue")
+            });
+            entry.Properties = ppr;
+
+            using (ODataMessageWriter omw = new ODataMessageWriter((IODataResponseMessage)msg, settings, extModel.Model))
+            {
+                var writer = omw.CreateODataFeedWriter(extModel.People);
+                writer.WriteStart(feed);
+                writer.WriteStart(entry);
+                writer.WriteEnd();
+                writer.WriteEnd();
+            }
+
+            stream.Seek(0, SeekOrigin.Begin);
+
+            using (var sr = new StreamReader(stream))
+            {
+                var payload = sr.ReadToEnd();
+                Console.WriteLine(payload);
+            }
+        }
+    }
+}
