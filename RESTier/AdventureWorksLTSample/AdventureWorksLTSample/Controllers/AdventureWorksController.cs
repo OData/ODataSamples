88eb37d36c3e3a519b75d02c43dbef90bde3fe2d
challenh <challenh@microsoft.com>
2015-04-22 01:31:20 -0700

    add samples of entity set filter convention & imperative view & their $count implementions.

--- RESTier/AdventureWorksLTSample/AdventureWorksLTSample/Controllers/AdventureWorksController.cs
+++ RESTier/AdventureWorksLTSample/AdventureWorksLTSample/Controllers/AdventureWorksController.cs
@@ -1,5 +1,7 @@
 ﻿using AdventureWorksLTSample.Models;
 using Microsoft.Restier.WebApi;
+using System.Threading.Tasks;
+using System.Web.Http;
 
 namespace AdventureWorksLTSample.Controllers
 {
@@ -8,6 +10,26 @@ namespace AdventureWorksLTSample.Controllers
         private AdventureWorksContext DbContext
         {
             get { return Domain.Context; }
+        }
+
+        /// <summary>
+        /// Attribute routing to enable $count
+        /// </summary>
+        /// <returns>IHttpActionResult of count</returns>
+        [System.Web.OData.Routing.ODataRouteAttribute("Products/$count")]
+        public async Task<IHttpActionResult> GetProductsCount()
+        {
+            return Ok(await this.Domain.GetProductsCountAsync());
+        }
+
+        /// <summary>
+        /// Attribute routing to enable $count
+        /// </summary>
+        /// <returns>IHttpActionResult of count</returns>
+        [System.Web.OData.Routing.ODataRouteAttribute("ColoredProducts/$count")]
+        public async Task<IHttpActionResult> GetColoredProductsCount()
+        {
+            return Ok(await this.Domain.GetColoredProductsCountAsync());
         }
     }
 }
