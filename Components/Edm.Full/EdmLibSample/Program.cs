2ce281c85a4efd0e426eb591ce6bbd0a07d031f8
Lewis Cheng <licheng@microsoft.com>
2015-04-28 23:41:35 -0700

    Update Edm.Full

---
+++ Components/Edm.Full/EdmLibSample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Microsoft.OData.Edm;
+using Microsoft.OData.Edm.Csdl;
+using Microsoft.OData.Edm.Validation;
+
+namespace EdmLibSample
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = new SampleModelBuilder();
+            var model = builder.GetModel();
+            var csdl = WriteModelToCsdl(model);
+            Console.WriteLine(csdl);
+        }
+
+        private static string WriteModelToCsdl(IEdmModel model)
+        {
+            var stringBuilder = new StringBuilder();
+            using (var writer = XmlWriter.Create(stringBuilder))
+            {
+                IEnumerable<EdmError> errors;
+                if (!model.TryWriteCsdl(writer, out errors))
+                {
+                    return null;
+                }
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ Components/Edm.Full/EdmLibSample/SampleModelBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.OData.Edm;
+using Microsoft.OData.Edm.Library;
+
+namespace EdmLibSample
+{
+    public class SampleModelBuilder
+    {
+        private readonly EdmModel _model = new EdmModel();
+        public IEdmModel GetModel()
+        {
+            return _model;
+        }
+    }
+}
