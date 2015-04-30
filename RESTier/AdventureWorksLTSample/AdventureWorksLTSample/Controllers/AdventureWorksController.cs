32b839d9586fc02ae10f087513480b3bf1781a46
Lewis Cheng <licheng@microsoft.com>
2015-04-29 22:07:36 -0700

    Sample code for 'Fallback'

--- RESTier/AdventureWorksLTSample/AdventureWorksLTSample/Controllers/AdventureWorksController.cs
+++ RESTier/AdventureWorksLTSample/AdventureWorksLTSample/Controllers/AdventureWorksController.cs
@@ -1,5 +1,6 @@
 ﻿using AdventureWorksLTSample.Models;
 using Microsoft.Restier.WebApi;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.OData.Routing;
@@ -12,6 +13,49 @@ namespace AdventureWorksLTSample.Controllers
         {
             get { return Domain.Context; }
         }
+
+        #region Fallback to Web API for OData
+        [ODataRoute("Customers({id})/CompanyName")]
+        [ODataRoute("Customers({id})/CompanyName/$value")]
+        public IHttpActionResult GetCustomerCompanyName(int id)
+        {
+            var customer = DbContext.Customers.Find(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(customer.CompanyName);
+        }
+
+        [ODataRoute("Customers/$count")]
+        public IHttpActionResult GetCustomersCount()
+        {
+            return Ok(this.DbContext.Customers.Count());
+        }
+
+        [HttpPut]
+        [ODataRoute("Products({id})/Color")]
+        public IHttpActionResult UpdateProductColor(int id, [FromBody] string color)
+        {
+            var product = DbContext.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            product.Color = color;
+            DbContext.SaveChanges();
+
+            return Ok(color);
+        }
+
+        [ODataRoute("Customers")]
+        public IHttpActionResult GetCustomers()
+        {
+            return Ok(this.DbContext.Customers.Where(c => c.CustomerID % 3 == 0));
+        }
+        #endregion
 
         /// <summary>
         /// Attribute routing to enable $count
