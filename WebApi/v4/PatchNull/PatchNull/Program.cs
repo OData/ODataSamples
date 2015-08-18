47717fb5638bbd4904f07dfba4c673368d99dedd
Congyong Su <cysu@microsoft.com>
2015-08-18 12:58:07 +0800

    Patch a complex type property

--- WebApi/v4/PatchNull/PatchNull/Program.cs
+++ WebApi/v4/PatchNull/PatchNull/Program.cs
@@ -27,6 +27,10 @@ namespace PatchNull
             dynamic delta = new JObject();
             delta.FirstName = "def";
             delta.LastName = null;
+            dynamic address = new JObject();
+            address.Region = null;
+            address.Street = "Lianhua";
+            delta.Address = address;
             var json = JsonConvert.SerializeObject(delta);
             var content = new StringContent(json, Encoding.Default, "application/json");
             var request = new HttpRequestMessage(new HttpMethod("PATCH"), "http://localhost/api/Customers(0)")
@@ -40,6 +44,10 @@ namespace PatchNull
             Console.WriteLine(result);
             var customer = JsonConvert.DeserializeObject<Customer>(result);
             Debug.Assert(customer.LastName == null);
+            Debug.Assert(customer.Address.Region == null);
+
+            // Patch a complex type property is to be supported: https://github.com/OData/WebApi/issues/135
+            // Debug.Assert(customer.Address.City == "Shanghai");
         }
     }
 
@@ -48,12 +56,28 @@ namespace PatchNull
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public Address Address { get; set; }
+    }
+
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
     }
 
     public class CustomersController : ODataController
     {
         private static readonly Customer[] _customers =
-            { new Customer { Id = 0, FirstName = "abc", LastName = "xyz" } };
+        {
+            new Customer
+            {
+                Id = 0,
+                FirstName = "abc",
+                LastName = "xyz",
+                Address = new Address { Region = "CN", City= "Shanghai", Street = "Zixing" }
+            }
+        };
 
         [AcceptVerbs("PATCH")]
         public IHttpActionResult Patch(int key, Delta<Customer> delta)
