bfb19fa18d6a9b9800d7e174e11efb3441d764d3
Sam Xu <saxu@microsoft.com>
2018-06-18 15:31:19 -0700

    add ODataActionSample for ASP.Net Core and classic

---
+++ WebApi/v7.x/ODataActionSample/ODataActionSample.AspNetFx/App_Start/WebApiConfig.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using System.Web.Http;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.OData.Edm;
+using ODataActionSample.Model;
+
+namespace ODataActionSample.AspNetFx
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapODataServiceRoute("OData", "odata", GetEdmModel());
+        }
+
+        // Builds the EDM model for the OData service, including the OData action definitions.
+        private static IEdmModel GetEdmModel()
+        {
+            var modelBuilder = new ODataConventionModelBuilder();
+            var moviesEntitySet = modelBuilder.EntitySet<Movie>("Movies");
+
+            // Now add actions.
+
+            // CheckOut
+            // URI: ~/odata/Movies(1)/ODataActionSample.Models.CheckOut
+            ActionConfiguration checkOutAction = modelBuilder.EntityType<Movie>().Action("CheckOut");
+            checkOutAction.ReturnsFromEntitySet<Movie>("Movies");
+
+            // ReturnMovie
+            // URI: ~/odata/Movies(1)/ODataActionSample.Models.Return
+            // Binds to a single entity; no parameters.
+            ActionConfiguration returnAction = modelBuilder.EntityType<Movie>().Action("Return");
+            returnAction.ReturnsFromEntitySet<Movie>("Movies");
+
+            // CheckOutMany action
+            // URI: ~/odata/Movies/ODataActionSample.Models.CheckOutMany
+            // Binds to a collection of entities.  This action accepts a collection of parameters.
+            ActionConfiguration checkOutManyAction = modelBuilder.EntityType<Movie>().Collection.Action("CheckOutMany");
+            checkOutManyAction.CollectionParameter<int>("MovieIDs");
+            checkOutManyAction.ReturnsCollectionFromEntitySet<Movie>("Movies");
+
+            // CreateMovie action
+            // URI: ~/odata/CreateMovie
+            // Unbound action. It is invoked from the service root.
+            ActionConfiguration createMovieAction = modelBuilder.Action("CreateMovie");
+            createMovieAction.Parameter<string>("Title");
+            createMovieAction.ReturnsFromEntitySet<Movie>("Movies");
+
+            modelBuilder.Namespace = typeof(Movie).Namespace;
+            return modelBuilder.GetEdmModel();
+        }
+    }
+}
---
+++ WebApi/v7.x/ODataActionSample/ODataActionSample.Model/MoviesContext.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+
+namespace ODataActionSample.Model
+{
+    public class MoviesContext
+    {
+        static List<Movie> _movies;
+        static MoviesContext()
+        {
+            _movies = new List<Movie>()
+            {
+                new Movie() { ID=1, Title = "Maximum Payback", Year = 1990 },
+                new Movie() { ID=2, Title = "Inferno of Retribution", Year = 2005 },
+                new Movie() { ID=3, Title = "Fatal Vengeance 2", Year = 2012 },
+                new Movie() { ID=4, Title = "Sudden Danger", Year = 2012 },
+                new Movie() { ID=5, Title = "Beyond Outrage", Year = 2014 },
+                new Movie() { ID=6, Title = "The Nut Job", Year = 2014 }
+            };
+        }
+
+        public List<Movie> Movies { get { return _movies; } }
+    }
+}
