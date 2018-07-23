38dab058faad9c9e12b3e1a75d153f928a05e1e5
Biao Li <biaol@microsoft.com>
2018-07-23 12:30:53 -0700

    WebApi 5.7 --> WebApi 7.0.1: ODataFunctionSample for classic and AspNetCore.

---
+++ WebApi/v7.x/ODataFunctionSample/ODataFunctionSample.Models/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ODataFunctionSample.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+    }
+}
