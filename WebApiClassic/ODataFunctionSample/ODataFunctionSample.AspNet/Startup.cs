3fb79125e288f3fb1f89cf86a1fd47a9dd95f20e
Biao Li <biaol@microsoft.com>
2018-07-26 09:50:00 -0700

    WebApi 5.7 --> WebApi 7.0.1: ODataFunctionSample for classic and core

---
+++ WebApiClassic/ODataFunctionSample/ODataFunctionSample.AspNet/Startup.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using System.Web.Http;
+using ODataFunctionSample.Models;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.OData.Edm;
+using Owin;
+
+namespace ODataFunctionSample.AspNet
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder builder)
+        {
+            var config = new HttpConfiguration();
+            config.MapODataServiceRoute(routeName: "odata route", routePrefix: "odata", model: GetEdmMode());
+            builder.UseWebApi(config);
+        }
+
+        private IEdmModel GetEdmMode()
+        {
+            ODataModelBuilder builder = new ODataConventionModelBuilder();
+
+            builder.EntitySet<Product>("Products");
+
+            var productType = builder.EntityType<Product>();
+
+            // Function bound to a collection
+            // Returns the most expensive product, a single entity
+            productType.Collection
+                .Function("MostExpensive")
+                .Returns<double>();
+
+            // Function bound to a collection
+            // Returns the top 10 product, a collection
+            productType.Collection
+                .Function("Top10")
+                .ReturnsCollectionFromEntitySet<Product>("Products");
+
+            // Function bound to a single entity
+            // Returns the instance's price rank among all products
+            productType
+                .Function("GetPriceRank")
+                .Returns<int>();
+
+            // Function bound to a single entity
+            // Accept a string as parameter and return a double
+            // This function calculate the general sales tax base on the 
+            // state
+            productType
+                .Function("CalculateGeneralSalesTax")
+                .Returns<double>()
+                .Parameter<string>("state");
+
+            // Unbound Function
+            builder.Function("GetSalesTaxRate")
+                .Returns<double>()
+                .Parameter<string>("state");
+
+            return builder.GetEdmModel();
+        }
+    }
+}
---
+++ WebApiCore/ODataFunctionSample/ODataFunctionSample.AspNetCore/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+namespace ODataFunctionSample.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+    }
+}
