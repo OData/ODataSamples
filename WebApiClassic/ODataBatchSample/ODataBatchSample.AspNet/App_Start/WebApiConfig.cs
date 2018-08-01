067871688af2c54714c5b1a414882f9ed42a7204
Biao Li <biaol@microsoft.com>
2018-07-31 17:20:02 -0700

    Add CopyRight headers.

--- WebApiClassic/ODataBatchSample/ODataBatchSample.AspNet/App_Start/WebApiConfig.cs
+++ WebApiClassic/ODataBatchSample/ODataBatchSample.AspNet/App_Start/WebApiConfig.cs
@@ -1,4 +1,7 @@
-﻿using ODataBatchSample.Models;
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using ODataBatchSample.Models;
 using System.Web.Http;
 using Microsoft.AspNet.OData.Batch;
 using Microsoft.AspNet.OData.Builder;
--- WebApiClassic/ODataBatchSample/ODataBatchSample.AspNet/Global.asax.cs
+++ WebApiClassic/ODataBatchSample/ODataBatchSample.AspNet/Global.asax.cs
@@ -1,4 +1,7 @@
-﻿using System.Data.Entity;
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using System.Data.Entity;
 using System.Web.Http;
 using System.Web.Routing;
 using ODataBatchSample.Models;
