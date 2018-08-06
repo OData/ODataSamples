ede003944b1eab43ab3b3239d1b84910ce5568bb
Biao Li <biaol@microsoft.com>
2018-08-06 10:16:26 -0700

    WebApi 5.7 -> WebApi 7: ODataModelReferenceSample for class and core

---
+++ WebApiClassic/ODataModelReferenceSample/SampleService2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SampleService2.WebApiApplication" Language="C#" %>
---
+++ WebApiCore/ODataModelReferenceSample/SampleService1/Model/Order.cs
@@ -0,0 +1,8 @@
+﻿namespace SampleService2.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+    }
+}
