5da352ed011846e561c1bdc9d1fb44c6dccb66bf
Biao Li <biaol@microsoft.com>
2018-08-06 11:37:48 -0700

    WebApi 6.0 --> WebApi 7.0.1: ODataUntypedSample for classic and core.

---
+++ WebApiCore/ODataUntypedSample/ODataUntypedSample.Client/Program.cs
@@ -0,0 +1,143 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Xml;
+
+namespace ODataUntypedSample.Client
+{
+    class Program
+    {
+        private static HttpClient client = new HttpClient();
+        private const string ServiceUrl = "https://localhost:44379";
+
+        static void Main(string[] args)
+        {
+            RunSample();
+
+            Console.WriteLine("Press any key to continue . . .");
+            Console.ReadKey();
+        }
+
+        public static void RunSample()
+        {
+            Console.WriteLine("1. Get Metadata.");
+            GetMetadata();
+
+            Console.WriteLine("\n2. Get Entity Set.");
+            GetEntitySet();
+
+            Console.WriteLine("\n3. Get Entity.");
+            GetEntity(5);
+
+            Console.WriteLine("\n4. Get Property From Entity.");
+            GetPropertyFromEntity(8);
+
+            Console.WriteLine("\n5. Post Entity.");
+            PostEntity(123);
+        }
+
+        public static void GetMetadata()
+        {
+            HttpResponseMessage response = client.GetAsync(ServiceUrl + "/odata/$metadata").Result;
+            PrintResponse(response);
+        }
+
+        public static void GetEntitySet()
+        {
+            HttpResponseMessage response = client.GetAsync(ServiceUrl + "/odata/Products?$filter=Id eq 1").Result;
+            PrintResponse(response);
+        }
+
+        public static void GetEntity(int id)
+        {
+            HttpResponseMessage response = client.GetAsync(ServiceUrl + String.Format("/odata/Products({0})", id)).Result;
+            PrintResponse(response);
+        }
+
+        public static void GetPropertyFromEntity(int id)
+        {
+            HttpResponseMessage response = client.GetAsync(ServiceUrl + String.Format("/odata/Products({0})/Category", id)).Result;
+            PrintResponse(response);
+        }
+
+        public static void PostEntity(int id)
+        {
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, ServiceUrl + "/odata/Products");
+            request.Content = new StringContent(JsonConvert.SerializeObject(new
+            {
+                Id = id,
+                Name = "Product " + id,
+                Price = 123.45,
+                Category = new
+                {
+                    Id = id % 5,
+                    Name = "Category " + (id % 5)
+                }
+            }),
+                Encoding.Default,
+                "application/json");
+
+            request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+
+            HttpResponseMessage response = client.SendAsync(request).Result;
+
+            PrintResponse(response);
+        }
+
+        public static void PrintResponse(HttpResponseMessage response)
+        {
+            response.EnsureSuccessStatusCode();
+
+            Console.WriteLine("Response:");
+            Console.WriteLine(response);
+
+            if (response.Content != null)
+            {
+                string payload = response.Content.ReadAsStringAsync().Result;
+
+                if (response.Content.Headers.ContentType.MediaType.Contains("xml"))
+                {
+                    Console.WriteLine(FormatXml(payload));
+                }
+                else if (response.Content.Headers.ContentType.MediaType.Contains("json"))
+                {
+                    JObject jobj = JObject.Parse(payload);
+                    Console.WriteLine(jobj);
+                }
+            }
+        }
+
+        private static string FormatXml(string source)
+        {
+            StringBuilder sb = new StringBuilder();
+            XmlTextWriter writer = null;
+
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(source);
+
+                writer = new XmlTextWriter(new StringWriter(sb));
+                writer.Formatting = System.Xml.Formatting.Indented;
+
+                doc.WriteTo(writer);
+            }
+            finally
+            {
+                if (writer != null)
+                {
+                    writer.Close();
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ WebApiCore/ODataUntypedSample/ODataUntypedSample/Startup.cs
@@ -0,0 +1,83 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OData.Edm;
+
+namespace ODataUntypedSample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOData();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc(routeBuilder
+                => routeBuilder.MapODataServiceRoute("odata", "odata", GetModel()));
+        }
+
+        private static IEdmModel GetModel()
+        {
+            EdmModel model = new EdmModel();
+
+            // Create and add product entity type.
+            EdmEntityType product = new EdmEntityType("NS", "Product");
+            product.AddKeys(product.AddStructuralProperty("Id", EdmPrimitiveTypeKind.Int32));
+            product.AddStructuralProperty("Name", EdmPrimitiveTypeKind.String);
+            product.AddStructuralProperty("Price", EdmPrimitiveTypeKind.Double);
+            model.AddElement(product);
+
+            // Create and add category entity type.
+            EdmEntityType category = new EdmEntityType("NS", "Category");
+            category.AddKeys(category.AddStructuralProperty("Id", EdmPrimitiveTypeKind.Int32));
+            category.AddStructuralProperty("Name", EdmPrimitiveTypeKind.String);
+            model.AddElement(category);
+
+            // Set navigation from product to category.
+            EdmNavigationPropertyInfo propertyInfo = new EdmNavigationPropertyInfo();
+            propertyInfo.Name = "Category";
+            propertyInfo.TargetMultiplicity = EdmMultiplicity.One;
+            propertyInfo.Target = category;
+            EdmNavigationProperty productCategory = product.AddUnidirectionalNavigation(propertyInfo);
+
+            // Create and add entity container.
+            EdmEntityContainer container = new EdmEntityContainer("NS", "DefaultContainer");
+            model.AddElement(container);
+
+            // Create and add entity set for product and category.
+            EdmEntitySet products = container.AddEntitySet("Products", product);
+            EdmEntitySet categories = container.AddEntitySet("Categories", category);
+            products.AddNavigationTarget(productCategory, categories);
+
+            return model;
+        }
+    }
+}
