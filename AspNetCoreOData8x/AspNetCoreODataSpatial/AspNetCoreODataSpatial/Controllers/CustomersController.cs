4fd9293ea7c518366ce9e31487823cf8f2ecca92
Kennedy Kang'ethe <kemunga@microsoft.com>
2022-08-01 09:08:01 +0300

    Customizing FIlterBinder to work with Spatial functions (#152)

---
+++ AspNetCoreOData8x/AspNetCoreODataSpatial/AspNetCoreODataSpatial/Controllers/CustomersController.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using AspNetCoreODataSpatial.Models;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using NetTopologySuite.Geometries;
+
+namespace AspNetCoreODataSpatial.Controllers
+{
+    public class CustomersController : ODataController
+    {
+        [EnableQuery]
+        public IQueryable<Customer> Get()
+        {
+            return DataSource.Instance.Customers.AsQueryable<Customer>();
+        }
+
+        [EnableQuery]
+        public Customer Get(int key)
+        {
+            Point redmond = new Point(-122.123889, 47.669444) { SRID = 4326 };
+            return DataSource.Instance.Customers.Where(b => b.Id == key).Single();
+        }
+    }
+}
---
+++ AspNetCoreOData8x/AspNetCoreODataSpatial/AspNetCoreODataSpatial/Models/DataSource.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using NetTopologySuite.Geometries;
+
+namespace AspNetCoreODataSpatial.Models
+{
+    public class DataSource
+    {
+        private static DataSource instance = new DataSource();
+        public static DataSource Instance => instance;
+        public List<Customer> Customers { get; set; }
+
+        private DataSource()
+        {
+            this.Reset();
+            this.Initialize();
+        }
+        public void Reset()
+        {
+            this.Customers = new List<Customer>();
+        }
+
+        public void Initialize()
+        {
+            Point seattle = new Point(-122.333056, 47.609722) { SRID = 4326 };
+            Point redmond = new Point(-122.123889, 47.669444) { SRID = 4326 };
+
+            this.Customers.AddRange(new List<Customer>()
+            {
+                new Customer()
+                {
+                    Id = 1,
+                    Name = "Customer 1",
+                    Loc = seattle
+                },
+                new Customer()
+                {
+                    Id = 2,
+                    Name = "Customer 2",
+                    Loc = redmond
+                }
+            });
+        }
+    }
+}
